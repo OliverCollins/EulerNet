6738a034b1c54390615cc216e889e4c12ed9647c
Oliver Collins <Oliver.Collins@Colorado.edu>
2018-06-10 18:54:00 -0600

    Create 019.py

---
+++ Complete/Problems 001 - 100/019.py
@@ -0,0 +1,13 @@
+# Programmer:    Oliver Collins
+# Date:          June 10th, 2018
+# Language:      Python
+
+'''
+----------------------------------
+'''
+
+# Counting Sundays
+# Solved
+
+# 12 months in a year * 100 years / 7 days in a week
+print(round(12 * 100 / 7))
