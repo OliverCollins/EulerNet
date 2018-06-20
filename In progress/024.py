ae0cfa3dff5274f6b63a1a6251cfa3c162269877
Oliver Collins <Oliver.Collins@Colorado.edu>
2018-06-19 21:03:33 -0600

    Create 024.py

---
+++ In progress/024.py
@@ -0,0 +1,15 @@
+# Programmer:    Oliver Collins
+# Date:          May 14th, 2018
+# Language:      Python
+
+'''
+-- -- -- -- -- -- -- -- -- -- --
+'''
+
+# Lexicographic permutations
+# Not Solved
+
+permutations = []
+
+while len(permutations) < 1000000:
+	pass
