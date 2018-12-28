650d78010725a458ce93bde05ce35fde07fd7e71
Oliver Collins <Oliver.Collins@Colorado.edu>
2018-12-27 22:13:07 -0800

    Update 056.py

--- complete/001-100/056.py
+++ complete/001-100/056.py
@@ -1,3 +1,14 @@
+# Programmer:    Oliver Collins
+# Date:          December 27th, 2018
+# Language:      Python
+
+'''
+----------------------------------
+'''
+
+# Powerful digit sum
+# Solved
+
 max_count = 0
 
 for a in range(2, 100):
